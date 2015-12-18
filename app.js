909ec489c308dcf05eb2d7602d2fcca3222f3053
Jon Harlem <jtharlem@gmail.com>
2015-12-18 12:07:05 -0800

    basic form for new.ejs and routes

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ var knex = require("./db/knex");
 //setting a poperty on the app
 app.set("view engine", "ejs");
 //middleware
-app.use(bodyParser.urlencoded({extender:true}));
+app.use(bodyParser.urlencoded({extended:true}));
 //tiny is the amount of information displayed when logging
 app.use(morgan('tiny'));
 
 //
-app.get('/student', function(req, res) {
+app.get('/students', function(req, res) {
 	//SELECT * FROM students and run a function to render the 
 	knex('students').then(function(studentsFromDB) {
 		// we want to respond with a file that shows all users
@@ -21,6 +21,15 @@ app.get('/student', function(req, res) {
 	});
 });
 
+app.get('/students/new', function(req, res) {
+	res.render("new");
+});
+
+app.post('/students', function(req, res) {
+	knex('students').insert(req.body).then(function(){
+		res.redirect('/students');
+	});
+});
 //start the server
 app.listen(3000, function() {
 	//log a message to make sure the server is running
