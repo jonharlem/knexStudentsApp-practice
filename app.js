7d792e8b7bb1e993742556eb3bac0e9074445886
Jon Harlem <jtharlem@gmail.com>
2015-12-18 11:31:03 -0800

    added basic idex file and added knex to app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var knex = require("./db/knex");
 
 //setting a poperty on the app
 app.set("view engine", "ejs");
@@ -10,6 +11,15 @@ app.use(bodyParser.urlcoded({extender:true}));
 //tiny is the amount of information displayed when logging
 app.use(morgan('tiny'));
 
+//
+app.get('/student', function(req, res) {
+	//SELECT * FROM students and run a function to render the 
+	knex('students').then(function(studentsFromDB) {
+		// we want to respond with a file that shows all users
+		//it is going to refer to what is sent back: students
+		res.render("index", {students:studentsFromDB});
+	});
+});
 
 //start the server
 app.listen(3000, function() {
